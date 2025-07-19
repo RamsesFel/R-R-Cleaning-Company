4a9d9981a9f56b751804265fe927b006bd3c2249
Ramses Feliciano <ramsesfeliciano@gmail.com>
2025-07-19 14:19:10 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './componants/about/about.component';
+import { ContactComponent } from './componants/contact/contact.component';
+import { ServicesComponent } from './componants/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AboutComponent },
+    {path: 'contact', component: ContactComponent},
+    {path: 'service', component: ServicesComponent}
+
+];
