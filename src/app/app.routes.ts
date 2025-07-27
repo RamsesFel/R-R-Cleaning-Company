a65a1604eb00b23864d33e75846521ea2eaeac90
Ramses Feliciano <ramsesfeliciano@gmail.com>
2025-07-27 16:04:08 -0400

    added gallery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './componants/about/about.component';
 import { ContactComponent } from './componants/contact/contact.component';
 import { ServicesComponent } from './componants/services/services.component';
+import { GalleryComponent } from './componants/gallery/gallery.component';
 
 export const routes: Routes = [
     {path: '', component: AboutComponent },
     {path: 'contact', component: ContactComponent},
-    {path: 'service', component: ServicesComponent}
+    {path: 'service', component: ServicesComponent},
+    {path: 'gallery', component: GalleryComponent}
 
 ];
